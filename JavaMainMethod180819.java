b295bf10d4ea1abd622765f96df127d799047555
uheuha <42325351+uheuha@users.noreply.github.com>
2018-08-19 23:43:40 -0700

    Update JavaMainMethod180819.java

--- JavaMainMethod180819.java
+++ JavaMainMethod180819.java
@@ -1,13 +1,21 @@
-class JavaMainMethod{
+public class JavaMainMethod{
 
-
-public sta void main(String args[]){
+        // try to change "static" to "sta" and run it in tutorialspoint.com; 
+        // observe the output errors and get familiar with errors; check the line number in the error report;
+    
+public static void main(String args[]){
 
 // "int" stands for integer; it is used to define the variable number1 as integer or a number;
-    // number1 below is calle a variable because you can change the value
+    // number1 below is called a variable because you can change the value 1 to any number/integer you want;
               // try to change this value/number and run this program by googling "tutorialspoint.com java"
+    
 int number1 = 1;
 
 System.out.println("This will print/output this sentence itself/mismo, plus the value of number1" + number1);
+    
+// try to remove this "}" below and execute it or run it at tutorialspoint.com;
+// observe the output errors and get familiar with errors; check the line number in the error report;
+    
+}
 
 }
